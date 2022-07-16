c6a7bc70304244693387bf2efa782576f8d5a989
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-07-16 13:33:39 +0300

    done with styling components

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function NoProject() {
+  return (
+    <Layout>
+      <div>NoProject</div>
+    </Layout>
+    
+  )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function about() {
+  return (
+    <Layout>
+      <div>about</div>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+        similique perspiciatis sapiente impedit cumque quasi cupiditate
+        recusandae molestiae laborum eum exercitationem, commodi dignissimos
+        illo modi iusto aut hic totam explicabo!
+      </h1>
+    </Layout>
+  )
+}
